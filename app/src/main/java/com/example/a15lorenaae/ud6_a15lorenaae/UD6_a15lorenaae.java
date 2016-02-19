18edca5b4e91ea6673d7878e44def7b25a8bf7a3
a15lorenaae <a15lorenaae@iessanclemente.net>
2016-02-19 16:01:56 +0100

    acabado

--- app/src/main/java/com/example/a15lorenaae/ud6_a15lorenaae/UD6_a15lorenaae.java
+++ app/src/main/java/com/example/a15lorenaae/ud6_a15lorenaae/UD6_a15lorenaae.java
@@ -1,14 +1,19 @@
 package com.example.a15lorenaae.ud6_a15lorenaae;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,71 +26,72 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class UD6_a15lorenaae extends FragmentActivity implements OnMapReadyCallback, LocationListener {
-    private LocationManager locManager;
-    private String provedor;
+
     private GoogleMap mMap;
+    Marker marcaactual;
+    private LocationManager locationManager;
+    private static String proveedor;
+
+    private void obterproveedores() {
+        Criteria filtro = new Criteria();
+        filtro.setAccuracy(Criteria.ACCURACY_FINE);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        proveedor = locationManager.getBestProvider(filtro, false);
+
+        if (proveedor == null) {
+            Toast.makeText(getApplicationContext(), "Non existen proveedores dispoñibles", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        if (!locationManager.isProviderEnabled(proveedor)) {
+            Toast.makeText(getApplicationContext(), "0 " + proveedor + " non esta activo", Toast.LENGTH_SHORT).show();
+                dialogoAlertaNonGPS();
+        } else {
+            Toast.makeText(getApplicationContext(), "proveedor atopado:" + proveedor, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void dialogoAlertaNonGPS() {
+    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setMessage(
+            "O GPS parece desactivado, queres activalo ?")
+            .setCancelable(false)
+    .setPositiveButton("Si",
+                               new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface arg0, int arg1) {
+            // TODO Auto-generated method stub
+            startActivity(new Intent(
+                    android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+        }
+    })
+            .setNegativeButton("Non", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO Auto-generated method stub
+                    dialog.cancel();
+                    UD6_a15lorenaae.this.finish();
+
+                }
+            });
+    final AlertDialog alert = builder.create();
+    alert.show();
+}
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ud6_a15lorenaae);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        obterprovedores();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(UD6_a15lorenaae.this);
+        obterproveedores();
 
     }
 
-    private void dialogoAlertaNonGPS() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(
-                "O GPS parece desactivado, queres activalo ?")
-                .setCancelable(false)
-                .setPositiveButton("Si",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface arg0, int arg1) {
-                                // TODO Auto-generated method stub
-                                startActivity(new Intent(
-                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-
-                            }
-                        })
-                .setNegativeButton("Non", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // TODO Auto-generated method stub
-                        dialog.cancel();
-                        UD6_a15lorenaae.this.finish();
-
-                    }
-                });
-        final AlertDialog alert = builder.create();
-        alert.show();
-    }
-    private void obterprovedores(){
-        Criteria filtro = new Criteria();
-        filtro.setAccuracy(Criteria.ACCURACY_FINE);
-
-        locManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-
-        provedor = locManager.getBestProvider(filtro, false);   // Se non está activo o avisamos e chamamos a activity para activalo
-//              provedor = LocationManager.NETWORK_PROVIDER;    => Exemplo concreto sen filtro
-
-        if (provedor==null){
-            Toast.makeText(getApplicationContext(), "Non existen provedores dispoñibles.", Toast.LENGTH_LONG).show();
-            finish();
-        }
-        if (!locManager.isProviderEnabled(provedor)){
-            Toast.makeText(getApplicationContext(), "O " + provedor + " non está activo", Toast.LENGTH_LONG).show();
-            dialogoAlertaNonGPS();
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "provedor atopado:" + provedor, Toast.LENGTH_LONG).show();
-        }
-    }
 
     /**
      * Manipulates the map once available.
@@ -100,39 +106,49 @@ public class UD6_a15lorenaae extends FragmentActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        locManager.requestLocationUpdates(provedor,0,100,UD6_a15lorenaae.this);
-        Toast.makeText(getApplicationContext(), "Comenzado a rexistrar...", Toast.LENGTH_SHORT).show();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 500, this);
 
-        Location last =locManager.getLastKnownLocation(provedor);
 
-        // Add a marker in Sydney and move the camera
-       /* LatLng localizacion = new LatLng(last.getLatitude(), last.getLongitude());*/
-        LatLng pos = new LatLng(42.879985, -8.544855);
-        mMap.addMarker(new MarkerOptions().position(pos).title("Lore").snippet("Estamos nesta posicion " + googleMap.getCameraPosition().target.toString()).icon(BitmapDescriptorFactory.fromResource(R.mipmap.lazo)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
-
+        LatLng pos=new LatLng(42.879985, -8.544855);
+        marcaactual=googleMap.addMarker(new MarkerOptions()
+                .position(pos)
+                .title("Lore")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.lazo)).snippet("Estamos en: " +pos.toString()));
 
 
 
     }
+
 
     @Override
     public void onLocationChanged(Location location) {
 
+        mMap.clear();
+        LatLng posicion  = new LatLng(location.getLatitude(),location.getLongitude());
+        marcaactual=mMap.addMarker(new MarkerOptions()
+                .position(posicion)
+                .title("Lore").icon(BitmapDescriptorFactory.fromResource(R.mipmap.lazo)).snippet("Estamos en " + posicion.toString()));
+                 location=locationManager.getLastKnownLocation(UD6_a15lorenaae.proveedor);
+       //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marcaactual.getPosition(),20));
+        if (location!=null) {
+            Toast.makeText(getApplicationContext(), "Ahora estamos en: " + posicion.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
-
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
+        Toast.makeText(getApplicationContext(), "O provedor " + provider + " cambió  de estado:" + String.valueOf(status), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        Toast.makeText(getApplicationContext(), "O provedor " + provider + " está activo", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"O proveedor "+provider+" esta activo",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        Toast.makeText(getApplicationContext(), "O provedor " + provider + " xa non está activo", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"O proveedor "+provider+" xa non esta activo",Toast.LENGTH_SHORT).show();
+
+
     }
 }
